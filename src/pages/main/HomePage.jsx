ee93f58b1186513b0418fd9a9ae614fe5f500605
cold <kholodihor@gmail.com>
2023-10-18 14:09:57 +0300

    feat: add structure to home page

--- src/pages/main/HomePage.jsx
+++ src/pages/main/HomePage.jsx
@@ -1,5 +1,27 @@
+import Header from '@/components/main/Header/Header';
+import Hero from '@/components/main/Hero/Hero';
+import Gallery from '@/components/main/Gallery/Gallery';
+import Achievements from '@/components/main/Achievements/Achievements';
+import History from '@/components/main/History/History';
+import Departments from '@/components/main/Departments/Departments';
+import Map from '@/components/main/Map/Map';
+import News from '@/components/main/News/News';
+import Footer from '@/components/main/Footer/Footer';
+
 const HomePage = () => {
-  return <h1>Київська дитяча школа мистецтв №2 ім. Михайла Вериківського</h1>;
+  return (
+    <>
+      <Header />
+      <Hero />
+      <History />
+      <Departments />
+      <News />
+      <Gallery />
+      <Achievements />
+      <Map />
+      <Footer />
+    </>
+  );
 };
 
 export default HomePage;
