635b7a60cb32aa84998b40d6ef23760f3918c481
BodNS <bondarenokn@gmail.com>
2023-10-20 16:41:12 +0200

    feat: add History component

--- src/components/main/History/History.jsx
+++ src/components/main/History/History.jsx
@@ -1,8 +1,29 @@
+//import React from "react";
+
+import NavLinkButton from "../../button/NavLinkButton"; 
+
+import styles from "./History.module.scss";
 
 const History = () => {
+  const url = 'https://www.example.com';
+  const buttonName = "Читати більше"; 
+
   return (
-    <div>History</div>
+    <div className={styles.wrapper}>
+      <div className={styles.info}>
+        <p className={styles.title}>
+          Історія школи
+        </p>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+        </p>
+        <NavLinkButton title={buttonName} link={url} />
+      </div>
+      <div className={styles.img_container}>
+        <img src="/school.png" alt="school-building"/>
+      </div>
+    </div>
   )
 }
 
-export default History
+export default History;
