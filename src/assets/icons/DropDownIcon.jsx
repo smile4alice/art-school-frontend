385de9d7801118347fe4ba268a5c83bf0212e6cf
Iryna Artymuk <artimyk@ukr.net>
2023-10-21 17:30:10 +0300

    feat(header      icons): creae drop down icon
    creat dropDown icon in icons add icon to header dropDown menu

---
+++ src/assets/icons/DropDownIcon.jsx
@@ -0,0 +1,18 @@
+const DropDownIcon = () => {
+  return (
+    <svg
+      width="8"
+      height="4"
+      viewBox="0 0 8 4"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M4.39504 3.98876L7.93005 0.452511L7.48817 0.011261L4.39504 3.10501L1.30073 0.011261L0.860107 0.453136L4.39504 3.98876Z"
+        fill="#120000"
+      />
+    </svg>
+  );
+};
+
+export default DropDownIcon;
--- src/components/main/Header/DropDownMenu/DropDownMenu.jsx
+++ src/components/main/Header/DropDownMenu/DropDownMenu.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { clsx } from 'clsx';
 import styles from './SelectDepertmemts.module.scss';
 import { Link } from 'react-router-dom';
+import DropDownIcon from '../../../../assets/icons/DropDownIcon';
 //<div className={`{styles.menu} ${open ? 'open' : ''}`}>
 const DropDownMenu = ({ type, items }) => {
   const [open, setOpen] = useState(false);
@@ -11,15 +12,19 @@ const DropDownMenu = ({ type, items }) => {
   };
 
   return (
-    <div className={styles.link}>
-      <span onClick={handleClick}>{type}</span>
-
+    <div className={styles.link} onClick={handleClick}>
+      <span>{type}</span>
+      <span>
+        <DropDownIcon />
+      </span>
       <div className={clsx(styles.menu, open ? styles.open : '')}>
         <ul className={styles.navList}>
           {items.map(({ name, to }) => (
-            <Link key={name} to={to}>
-              {name}
-            </Link>
+            <li key={name}>
+              <Link key={name} to={to}>
+                {name}
+              </Link>
+            </li>
           ))}
         </ul>
       </div>
