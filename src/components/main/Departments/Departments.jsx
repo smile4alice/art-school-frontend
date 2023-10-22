ea4e3482172db993bd66fc60302c1c2b5a4f3f21
BodNS <bondarenokn@gmail.com>
2023-10-22 12:46:15 +0200

    refactor: add button

--- src/components/main/Departments/Departments.jsx
+++ src/components/main/Departments/Departments.jsx
@@ -1,4 +1,3 @@
-import NavLinkButton from '../../ui/NavLinkButton/NavLinkButton';
 import Department from './Department/Department';
 import Container from '../../Container/Container';
 
@@ -12,19 +11,22 @@ import styles from './Departments.module.scss';
 
 const Departments = () => {
   const link = 'hppp:\\example.com';
-  const buttonTitle = 'завантажити заяву';
   const musicDepartment = 'Музичне відділення';
   const artDepartment = 'Образотворче відділення';
   const horeografyDepartment = 'Хореографічне відділення';
   const theaterDepartment = 'Театральне відділення';
   const vocalDepartment = 'Вокально-хорове відділення';
 
+  const downloadFile = async () => {
+    
+  };
+
   return (
     <Container>
       <div className={styles.wrapper}>
         <p className={styles.title}>Відділення</p>
-        <div className={styles.button}>
-          <NavLinkButton title={buttonTitle} link={link} />
+        <div className={styles.buttonContainer}>
+          <button onClick={downloadFile} className={styles.buttonStyle}>завантажити заяву</button>
         </div>
         <div className={styles.departmentsContainer}>
           <Department title={musicDepartment} link={link} img={musicImg} />
