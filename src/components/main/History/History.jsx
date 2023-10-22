590dbb9d2b96c3643e580acb0b8e263f85747d9d
BodNS <bondarenokn@gmail.com>
2023-10-22 13:56:26 +0200

    refactor: change flex on grid

--- src/components/main/History/History.jsx
+++ src/components/main/History/History.jsx
@@ -1,29 +1,32 @@
-//import React from "react";
+import NavLinkButton from '../../ui/NavLinkButton/NavLinkButton';
 
-import NavLinkButton from "../../button/NavLinkButton"; 
-
-import styles from "./History.module.scss";
+import styles from './History.module.scss';
 
 const History = () => {
   const url = 'https://www.example.com';
-  const buttonName = "Читати більше"; 
+  const buttonName = 'Читати більше';
 
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.info}>
-        <p className={styles.title}>
-          Історія школи
-        </p>
-        <p className={styles.text}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-        </p>
-        <NavLinkButton title={buttonName} link={url} />
+      <div className={styles.wrapper}>
+        <div className={styles.info}>
+          <p className={`${styles.title} sectionTitle`}>Історія школи</p>
+          <p className={styles.text}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur.
+          </p>
+        </div>
+        <div className={styles.buttonContainer}>
+          <NavLinkButton title={buttonName} link={url} />
+        </div>
+        <div className={styles.img_container}>
+          <img src="/school.png" alt="school-building" />
+        </div>
       </div>
-      <div className={styles.img_container}>
-        <img src="/school.png" alt="school-building"/>
-      </div>
-    </div>
-  )
-}
+  );
+};
 
 export default History;
