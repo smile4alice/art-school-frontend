b03d5217032ea1a5c07bd5ee22fa750471b1ad81
Iryna Artymuk <artimyk@ukr.net>
2023-10-23 11:13:07 +0300

    chore(header): remove unused const

--- src/components/main/Header/Header.jsx
+++ src/components/main/Header/Header.jsx
@@ -2,10 +2,8 @@ import HeaderContacts from './HeaderContacts';
 import HeaderNavigation from './HeaderNavigation';
 
 import styles from './Header.module.scss';
-import { useEffect } from 'react';
 
 const Header = () => {
-
   return (
     <header className={styles.headerWrapper}>
       <HeaderContacts />
