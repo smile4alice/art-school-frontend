bdc3cf947ec4e81db9a75c06c6a81cde879997be
Dima Telechkun <telechkun93@gmail.com>
2023-10-25 02:44:05 +0300

    navigation and pagination styling

--- src/components/main/Hero/Hero.jsx
+++ src/components/main/Hero/Hero.jsx
@@ -1,76 +1,49 @@
-// import imageHero1 from '/public/hero/building.JPG';
-// import imageHero2 from '/public/hero/art-building.JPG';
-// import imageHero3 from '/public/hero/platform.JPG';
 // import React, { useRef, useState } from 'react';
-// import Container from '@/components/Container/Container';
+// import 'swiper/swiper-bundle.min.css';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Keyboard } from 'swiper/modules';
-import { hero } from '@/constants/hero.js';
+import Data from '@/constants/hero.json';
 
 import 'swiper/css';
+import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import styles from './Hero.module.scss';
 
 const Hero = () => {
   return (
-    // <Container>
     <section className={styles.HeroSection}>
       <Swiper
         style={{
           '--swiper-navigation-color': '#d66600',
-          '--swiper-navigation-background-color': '#00ff0d',
-          '--swiper-navigation-top-offset': '50%',
-          '--swiper-navigation-size': '20px',
-          '--swiper-navigation-weight': '900',
-          '--swiper-pagination-color': '#ffffff',
-          '--swiper-pagination-active-color': '#007bff',
+          '--swiper-navigation-sides-offset': '2.8rem',
+          //   '--swiper-navigation-top-offset': '50%',
+          '--swiper-navigation-size': '1.25rem',
+          //   '--swiper-pagination-color': '#ffffff',
+          '--swiper-pagination-bullet-horizontal-gap': '1rem',
         }}
         navigation={true}
-        pagination={{
-          dynamicBullets: true,
-        }}
+        pagination={{ clickable: true }}
         // navigation={{
         //   prevEl: '.swiper-button-prev',
         //   nextEl: '.swiper-button-next',
         // }}
-        mousewheel={true}
+
         keyboard={true}
         slidesPerView={1}
         spaceBetween={2}
         modules={[Navigation, Pagination, Keyboard]}
         className={styles.SliderStyle}
-        // pagination="true"
-        // effect="coverflow"
-        // grab-cursor="true"
-        // centered-slides="true"
-        // slides-per-view="auto"
-        // coverflow-effect-rotate="50"
-        // coverflow-effect-stretch="0"
-        // coverflow-effect-depth="100"
-        // coverflow-effect-modifier="1"
-        // coverflow-effect-slide-shadows="true"
       >
-        {hero.map((some, index) => (
+        {Data.map((result, index) => (
           <SwiperSlide
             key={index}
             // className={styles.Slide}
           >
-            <div
-              className={styles.HeroImage}
-              style={{
-                backgroundImage: `url("hero/building.jpg")`,
-                backgroundSize: 'cover',
-                backgroundPosition: 'center',
-                width: '100%',
-                height: '615px', // Set the height as per your design
-              }}
-            />
-            {/* <img src="/hero/building.jpg" alt="" className={styles.HeroImage} /> */}
+            <img src={result.img} alt="" className={styles.HeroImage} />
           </SwiperSlide>
         ))}
       </Swiper>
     </section>
-    // </Container>
   );
 };
 
