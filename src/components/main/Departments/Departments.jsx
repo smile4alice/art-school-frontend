1a9105455204e63fd506d45e4e1d32b367712f8f
BodNS <bondarenokn@gmail.com>
2023-10-25 00:13:10 +0200

    feat: add Container

--- src/components/main/Departments/Departments.jsx
+++ src/components/main/Departments/Departments.jsx
@@ -1,10 +1,11 @@
 import Department from './Department/Department';
+import Container from '@/components/Container/Container';
 
-import musicImg from "/departments/music.png";
-import artImg from "/departments/art.png";
-import choreografyImg from "/departments/horeografy.png";
-import theaterImg from "/departments/theater.png";
-import vocalImg from "/departments/vocal.png";
+import musicImg from '/departments/music.png';
+import artImg from '/departments/art.png';
+import choreografyImg from '/departments/horeografy.png';
+import theaterImg from '/departments/theater.png';
+import vocalImg from '/departments/vocal.png';
 
 import styles from './Departments.module.scss';
 
@@ -16,24 +17,27 @@ const Departments = () => {
   const theaterDepartment = 'Театральне відділення';
   const vocalDepartment = 'Вокально-хорове відділення';
 
-  const downloadFile = async () => {
-    
-  };
+  const downloadFile = async () => {};
 
   return (
+    <Container>
       <div className={styles.wrapper}>
         <p className={`${styles.title} sectionTitle`}>Відділення</p>
         <div className={styles.buttonContainer}>
-          <button onClick={downloadFile} className={styles.buttonStyle}>завантажити заяву</button>
+          <button onClick={downloadFile} className={styles.buttonStyle}>
+            завантажити заяву
+          </button>
         </div>
         <div className={styles.departmentsContainer}>
           <Department title={musicDepartment} link={link} img={musicImg} />
           <Department title={vocalDepartment} link={link} img={vocalImg} />
-          <Department title={choreografyDepartment} link={link} img={choreografyImg} />
+          <Department title={choreografyDepartment} link={link} img={choreografyImg}
+          />
           <Department title={artDepartment} link={link} img={artImg} />
           <Department title={theaterDepartment} link={link} img={theaterImg} />
         </div>
       </div>
+    </Container>
   );
 };
 
