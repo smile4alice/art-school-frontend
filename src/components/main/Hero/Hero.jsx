5fc62b29befe2be8fa393f685370f5a472d37eea
Dima Telechkun <telechkun93@gmail.com>
2023-10-25 03:59:57 +0300

    hero title has been applied.h1 has been choosen.

--- src/components/main/Hero/Hero.jsx
+++ src/components/main/Hero/Hero.jsx
@@ -16,10 +16,9 @@ const Hero = () => {
         style={{
           '--swiper-navigation-color': '#d66600',
           '--swiper-navigation-sides-offset': '2.8rem',
-          //   '--swiper-navigation-top-offset': '50%',
           '--swiper-navigation-size': '1.25rem',
-          //   '--swiper-pagination-color': '#ffffff',
           '--swiper-pagination-bullet-horizontal-gap': '1rem',
+          '--swiper-pagination-bottom': '1rem',
         }}
         navigation={true}
         pagination={{ clickable: true }}
@@ -27,7 +26,6 @@ const Hero = () => {
         //   prevEl: '.swiper-button-prev',
         //   nextEl: '.swiper-button-next',
         // }}
-
         keyboard={true}
         slidesPerView={1}
         spaceBetween={2}
@@ -35,14 +33,20 @@ const Hero = () => {
         className={styles.SliderStyle}
       >
         {Data.map((result, index) => (
-          <SwiperSlide
-            key={index}
-            // className={styles.Slide}
-          >
-            <img src={result.img} alt="" className={styles.HeroImage} />
+          <SwiperSlide key={index} className={styles.SlideStyle}>
+            <img src={result.img} alt="hero" className={styles.HeroImage} />
           </SwiperSlide>
         ))}
       </Swiper>
+      <div className={styles.HeroTitle}>
+        <h1 className={styles.HeroPrimaryTitle}>
+          Київська дитяча школа мистецтв №2
+          <span className={styles.HeroSecondaryTitle}>
+            {' '}
+            імені Михайла Вериківського
+          </span>
+        </h1>
+      </div>
     </section>
   );
 };
