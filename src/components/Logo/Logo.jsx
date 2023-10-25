9fd28e70a364fe24741fe2414295a0e12d004658
Iryna Artymuk <artimyk@ukr.net>
2023-10-25 15:58:08 +0300

    fix(logo): change logo content

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -3,15 +3,13 @@ import LogoIcon from '../Icons/LogoIcon';
 import styles from './Logo.module.scss';
 const Logo = () => {
   return (
-      <Link to="/" className={styles.logoWrapper}>
-         <LogoIcon/>
-         <p className={styles.logoText}>
-        Київська дитяча школа мистецтв №2 <br />
-        <span> ім. Михайла Вериківського</span>
-      </p>
-      </Link>
-     
-    
+    <Link to="/" className={styles.logoWrapper}>
+      <LogoIcon />
+      <div className={styles.logoText}>
+        <p>Київська дитяча школа мистецтв №2</p>
+        <span> ім. М. I. Вериківського</span>
+      </div>
+    </Link>
   );
 };
 
