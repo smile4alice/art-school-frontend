3c7d3168a6daa5bd692277f3bda2e177b29c1794
Baza Trainee Ukraine <132754508+baza-trainee@users.noreply.github.com>
2023-11-13 13:11:12 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import Layout from './components/layout/Layout';
 import HomePage from './pages/main/HomePage';
 import Page404 from './pages/page_404/Page404';
 // import Contacts from './pages/main/contacts/Contacts';
-import Gallery from './pages/main/gallery/Gallery';
-import PostersPage from './pages/PostersPage/PostersPage';
+// import Gallery from './pages/main/gallery/Gallery';
+// import PostersPage from './pages/PostersPage/PostersPage';
 
 const App = () => {
   return (
@@ -12,10 +12,10 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
-        <Route path="/posters" element={<PostersPage/>} />
+{/*         <Route path="/posters" element={<PostersPage/>} /> */}
           <Route path="*" element={<Page404 />} />
 {/*           <Route path="/contacts" element={<Contacts />} /> */}
-          <Route path="/gallery" element={<Gallery />} />
+{/*           <Route path="/gallery" element={<Gallery />} /> */}
         </Route>
       </Routes>
     </Router>
