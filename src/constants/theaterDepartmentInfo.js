c224357d411a96e16529e7c4d317156eee08bb0f
Iryna Artymuk <artimyk@ukr.net>
2023-11-16 18:56:20 +0200

    feat(theater): add slider achivments

--- src/constants/theaterDepartmentInfo.js
+++ src/constants/theaterDepartmentInfo.js
@@ -1,20 +1,63 @@
 export const theaterDepartmentInfo = [
   {
     id: 1,
-    url: '/images/dafault.png',
+    url: '/defaultImg.png',
     description:
       'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione asperiores in commodi impedit omnis fuga ipsum! Dolore perspiciatis ex repellendus nesciunt. Nostrum delectus nisi harum, quisquam commodi temporibus recusandae cum earum. Vitae nesciunt sit quisquam nulla reprehenderit libero odit nemo veniam dicta, eveniet doloremque repellendus, sapiente natus eos quo molestiae id accusantium odio voluptas! Illum doloribus aliquid aut animi vel ipsa necessitatibus officia suscipit incidunt itaque distinctio, repudiandae ullam voluptatibus autem soluta provident quaerat fugit facilis accusamus. Iure eius molestiae inventore quibusdam dolore. Cum odio quam dolore earum rem beatae dolor dolorum at. Nam, repellendus exercitationem molestias reiciendis voluptatem ullam?',
   },
   {
     id: 2,
-    url: '/images/dafault.png',
+    url: '/defaultImg.png',
     description:
       'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione asperiores in commodi impedit omnis fuga ipsum! Dolore perspiciatis ex repellendus nesciunt. Nostrum delectus nisi harum, quisquam commodi temporibus recusandae cum earum. Vitae nesciunt sit quisquam nulla reprehenderit libero odit nemo veniam dicta, eveniet doloremque repellendus, sapiente natus eos quo molestiae id accusantium odio voluptas! Illum doloribus aliquid aut animi vel ipsa necessitatibus officia suscipit incidunt itaque distinctio, repudiandae ullam voluptatibus autem soluta provident quaerat fugit facilis accusamus. Iure eius molestiae inventore quibusdam dolore. Cum odio quam dolore earum rem beatae dolor dolorum at. Nam, repellendus exercitationem molestias reiciendis voluptatem ullam?',
   },
   {
     id: 3,
-    url: '/images/dafault.png',
+    url: '/defaultImg.png',
     description:
       'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione asperiores in commodi impedit omnis fuga ipsum! Dolore perspiciatis ex repellendus nesciunt. Nostrum delectus nisi harum, quisquam commodi temporibus recusandae cum earum. Vitae nesciunt sit quisquam nulla reprehenderit libero odit nemo veniam dicta, eveniet doloremque repellendus, sapiente natus eos quo molestiae id accusantium odio voluptas! Illum doloribus aliquid aut animi vel ipsa necessitatibus officia suscipit incidunt itaque distinctio, repudiandae ullam voluptatibus autem soluta provident quaerat fugit facilis accusamus. Iure eius molestiae inventore quibusdam dolore. Cum odio quam dolore earum rem beatae dolor dolorum at. Nam, repellendus exercitationem molestias reiciendis voluptatem ullam?',
+  },
+];
+
+export const theaterDepartmentAchivments = [
+  {
+    id: 1,
+    url: '/defaultImg.png',
+    text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. ',
+  },
+  {
+    id: 2,
+    url: '/defaultImg.png',
+    text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. ',
+  },
+  {
+    id: 3,
+    url: '/defaultImg.png',
+    text: 'Lorem ipsum dolor sit amet consectetur,Lorem ipsum dolor sit amet consectetur,Lorem ipsum dolor sit amet consectetur,Lorem ipsum dolor sit amet consectetur, . ',
+  },
+  {
+    id: 4,
+    url: '/defaultImg.png',
+    text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. ',
+  },
+  {
+    id: '5',
+    url: '/achievements/img1.png',
+    text: 'Тетяна Мазур зайняла перше місце в міжнародному конкурсі 1',
+  },
+  {
+    id: '6',
+    url: '/achievements/img2.png',
+    text: 'Тетяна Мазур зайняла перше місце в міжнародному конкурсі 2',
+  },
+  {
+    id: '7',
+    url: '/achievements/img3.png',
+    text: 'Тетяна Мазур зайняла перше місце в міжнародному конкурсі 3',
+  },
+  {
+    id: '8',
+    url: '/achievements/img1.png',
+    text: 'Тетяна Мазур зайняла перше місце в міжнародному конкурсі 4',
   },
 ];
--- src/pages/main/theater/Theater.jsx
+++ src/pages/main/theater/Theater.jsx
@@ -1,12 +1,35 @@
 import Container from '@/components/Container/Container';
-// import { theaterDepartmentInfo } from '@/constants/theaterDepartmentInfo';
+import {
+  theaterDepartmentInfo,
+  theaterDepartmentAchivments,
+} from '@/constants/theaterDepartmentInfo';
 
 import styles from './Theater.module.scss';
+
+import AchievementsSlider from '@/components/Slider/achievementsSlider/achievementsSlider';
+
 const Theater = () => {
   return (
-    <Container>
-      <section className={styles.contentWrapper}></section>
-    </Container>
+    <section>
+      <Container>
+        <div className={styles.contentWrapper}>
+          <ul>
+            {theaterDepartmentInfo.map(item => (
+              <li key={item.id}>
+                <div>
+                  <img src={item.url} alt="" />
+                  <p> {item.description}</p>
+                </div>
+              </li>
+            ))}
+          </ul>
+
+          
+            <AchievementsSlider data={theaterDepartmentAchivments} />
+          
+        </div>
+      </Container>
+    </section>
   );
 };
 
