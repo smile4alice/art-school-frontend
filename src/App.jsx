d85b3b4ac4cf370c4c3e0858f931750caf31a12f
BodNS <bondarenokn@gmail.com>
2023-11-18 12:38:02 +0100

    merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/layout/Layout';
-<<<<<<< HEAD
 import HomePage from './pages/main/HomePage';
 import Page404 from './pages/page_404/Page404';
 import Contacts from './pages/main/contacts/Contacts';
 import Gallery from './pages/main/gallery/Gallery';
 import AllNews from './pages/main/allnews/AllNews';
 import OneNews from './pages/main/onenews/OneNews';
-=======
-import HomePage from '@/pages/main/HomePage';
-import Page404 from '@/pages/page_404/Page404';
-import Contacts from '@/pages/main/contacts/Contacts';
-import Gallery from '@/pages/main/gallery/Gallery';
 import PostersPage from '@/pages/main/posters/PostersPage';
 import Cooperation from './pages/main/cooperation/Cooperation';
->>>>>>> dev
 
 const App = () => {
   return (
@@ -25,13 +18,10 @@ const App = () => {
           <Route path="*" element={<Page404 />} />
           <Route path="/contacts" element={<Contacts />} />
           <Route path="/gallery" element={<Gallery />} />
-<<<<<<< HEAD
           <Route path="/news" element={<AllNews />} />
           <Route path="/news/:id" element={<OneNews />} />
-=======
           <Route path="/posters" element={<PostersPage />} />
           <Route path="/cooperation" element={<Cooperation />} />
->>>>>>> dev
         </Route>
       </Routes>
     </Router>
