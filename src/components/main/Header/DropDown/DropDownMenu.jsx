423e34a72a1654b15ab7866deb172702e71f5b2c
Iryna Artymuk <artimyk@ukr.net>
2023-11-24 15:49:08 +0200

    feat(dropdownmenu): creat new DropDownMenu

--- src/components/main/Header/DropDown/DropDownMenu.jsx
+++ src/components/main/Header/DropDown/DropDownMenu.jsx
@@ -1,63 +1,124 @@
 import { clsx } from 'clsx';
 import styles from './DropDown.module.scss';
 import { Link } from 'react-router-dom';
+import { useMediaQuery } from 'react-responsive';
+import { HashLink } from 'react-router-hash-link';
 
-const DropDownMenu = ({ type, items, handleClick, open, toggleBurgerMenu }) => {
-  // console.log('open: ', open);
+const DropDownMenu = ({
+  showDepartment,
+  departmemts,
+  aboutSchool,
+  showAboutUs,
+  toggleBurgerMenu,
+  handleClickDepartment,
+  handleClickAboutSchool,
+  handleCloseAll,
+}) => {
+  const isDesktop = useMediaQuery({ minWidth: 1240 });
 
   return (
-    <div className={styles.dropDown}>
-      <div
-        data-element-id="dropdown"
-        className={styles.dropDownNameWrapper}
-        onClick={() => handleClick(open)}
+    <>
+      <HashLink
+        className={clsx(styles.dropDown, !isDesktop && styles.dropDownMobile)}
+        onClick={handleClickDepartment}
+        onMouseEnter={handleClickDepartment}
+        onMouseLeave={handleCloseAll}
+        scroll={el => {
+          if (isDesktop) {
+            el.scrollIntoView({
+              behavior: 'smooth',
+              block: 'end',
+            });
+          }
+        }}
+        to="/#departmens"
       >
-        <span
-          className={clsx(styles.dropDownName, open ? styles.open : '')}
-          data-element-id="dropdown"
-        >
-          {type}
-        </span>
-        {!open ? (
-          <span
-            className={styles.dropDown_iconDown}
-            data-element-id="dropdown"
-          ></span>
-        ) : (
-          <span
-            className={styles.dropDown_iconUp}
-            data-element-id="dropdown"
-          ></span>
-        )}
-      </div>
+        <div className={styles.dropDownNameWrapper}>
+          <span className={clsx(styles.dropDownName, open ? styles.open : '')}>
+            {/* {type} */}
+            Відділення
+          </span>
 
-      {open && (
-        <ul
-          className={clsx(
-            styles.menu,
-            open ? styles.open : '',
-            type === 'Відділення' ? styles.departmentsMenu : styles.aboutUsMenu
+          {!showDepartment ? (
+            <span className={styles.dropDown_iconDown}></span>
+          ) : (
+            <span className={styles.dropDown_iconUp}></span>
           )}
-        >
-          {items.map(({ name, to }) => (
-            <li className={styles.menuItem} key={name}>
-              <Link
-                className={clsx(styles.menulink, open ? styles.open : '')}
-                key={name}
-                to={to}
-                onClick={() => {
-                  toggleBurgerMenu();
+        </div>
 
-                  handleClick(open);
-                }}
-              >
-                {name}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
+        {showDepartment && (
+          <ul
+            className={clsx(
+              styles.menu,
+              open ? styles.open : '',
+              styles.departmentsMenu
+            )}
+          >
+            {departmemts.map(({ name, to }) => (
+              <li className={styles.menuItem} key={name}>
+                <Link
+                  className={clsx(styles.menulink, open ? styles.open : '')}
+                  key={name}
+                  to={to}
+                  onClick={() => {
+                    toggleBurgerMenu();
+                  }}
+                >
+                  {name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        )}
+      </HashLink>
+
+      <Link
+        className={clsx(styles.dropDown, !isDesktop && styles.dropDownMobile)}
+        to="/aboutSchool"
+        onClick={handleClickAboutSchool}
+        onMouseEnter={handleClickAboutSchool}
+        onMouseLeave={handleCloseAll}
+      >
+        <div className={styles.dropDownNameWrapper}>
+          <span className={clsx(styles.dropDownName, open ? styles.open : '')}>
+            {/* {type} */}
+            Про школу
+          </span>
+
+          {!showAboutUs ? (
+            <span className={styles.dropDown_iconDown}></span>
+          ) : (
+            <span className={styles.dropDown_iconUp}></span>
+          )}
+        </div>
+
+        {showAboutUs && (
+          <ul
+            className={clsx(
+              styles.menu,
+              open ? styles.open : '',
+
+              styles.aboutUsMenu
+            )}
+          >
+            {aboutSchool.map(({ name, to }) => (
+              <li className={styles.menuItem} key={name}>
+                <Link
+                  className={clsx(styles.menulink, open ? styles.open : '')}
+                  key={name}
+                  to={to}
+                  onClick={() => {
+                    toggleBurgerMenu();
+                  }}
+                >
+                  {name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        )}
+      </Link>
+    </>
   );
 };
 
