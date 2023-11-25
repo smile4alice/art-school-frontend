c23c9d056f582f45f54692e83b3fd51196a9193b
Iryna Artymuk <artimyk@ukr.net>
2023-11-25 15:58:13 +0200

    fix(app): bG#19
    fix Хедер - Відбувається перехід на головну сторінку при натисканні на розділ "Наші Події" у
    навігаційному меню

--- src/pages/main/aboutSchool/AboutSchool.jsx
+++ src/pages/main/aboutSchool/AboutSchool.jsx
@@ -3,7 +3,9 @@ import Container from '@/components/Container/Container';
 const AboutSchool = () => {
   return (
     <Container>
-      <h1>наша школа</h1>
+      <div style={{ height: '100vh' }}>
+        <h1>наша школа</h1>
+      </div>
     </Container>
   );
 };
