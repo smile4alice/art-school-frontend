868bec61895e0f5a1196ac3616609a6e3de4def1
Olena Kasianenko <kasianenko.elena.a@gmail.com>
2023-11-26 13:26:48 +0100

    Admin panel components: BasicContainerLogin, Heading, InputSm

---
+++ src/components/admin-components/Login/BasicContainerLogin/BasicContainerLogin.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Heading from "../Heading/Heading";
+import styles from "./BasicContainerLogin.module.scss";
+
+
+export const BasicContainerLogin = () => {
+ 
+  return (
+    <div  className={styles.basicContainerLoginWrap}></div>   
+  );
+};
+export default BasicContainerLogin;
+
