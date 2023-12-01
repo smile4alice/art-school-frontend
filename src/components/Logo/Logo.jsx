5d9bf4e9ae2e414285bb798af397b5038a26847a
cold <kholodihor@gmail.com>
2023-12-01 12:32:59 +0200

    feat: add scroll to top

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -2,11 +2,16 @@ import { Link } from 'react-router-dom';
 import LogoIcon from '../Icons/LogoIcon';
 import styles from './Logo.module.scss';
 const Logo = () => {
+  const scrollToTop = () => {
+    window.scrollTo(0, 0);
+  };
+
   return (
     <Link
       aria-label="company logo navigation to home page"
       to="/"
       className={styles.logoWrapper}
+      onClick={scrollToTop}
     >
       <LogoIcon />
       <div className={styles.logoText}>
