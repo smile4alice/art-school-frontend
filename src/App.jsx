a061a5933277a2f1a9905f913c7e7b4c6e2f921c
cold <kholodihor@gmail.com>
2023-12-03 11:18:28 +0200

    feat(departments): add departments pages

--- src/App.jsx
+++ src/App.jsx
@@ -10,15 +10,14 @@ import PostersPage from '@/pages/main/posters/PostersPage';
 import Cooperation from './pages/main/cooperation/Cooperation';
 //import Theater from './pages/main/theater/Theater';
 import AboutSchool from './pages/main/aboutSchool/AboutSchool';
-import DepartmentPage from './pages/department_page/DepartmentPage';
-
+import DepartmentPage from './pages/departments/DepartmentPage';
 
 import LoginLayout from './components/admin-components/Login/LoginLayout/LoginLayout';
 import AdminSharedLayout from './components/admin-components/AdminSharedLayout/AdminSharedLayout';
 import AdminDashboard from './components/admin-components/AdminDashboard/AdminDashboard';
 
 import SlidersPageAdmin from './pages/admin-pages/SlidersAdminAdmin/SlidersPageAdmin';
-import AddSlidersPageAdmin from './pages/admin-pages/SlidersAdminAdmin/AddSlidersPageAdmin'
+import AddSlidersPageAdmin from './pages/admin-pages/SlidersAdminAdmin/AddSlidersPageAdmin';
 import NewsPageAdmin from './pages/admin-pages/NewsAdmin/AddNewsPageAdmin';
 import PostersPageAdmin from './pages/admin-pages/PostersAdmin/PostersPageAdmin';
 import GalleryPageAdmin from './pages/admin-pages/GalleryAdmin/GalleryAdmin/GalleryPageAdmin';
@@ -46,19 +45,75 @@ const App = () => {
           <Route path="/about_school" element={<AboutSchool />} />
           <Route path="/about_school_history'" element={<AboutSchool />} />
           <Route path="/about_school_museum" element={<AboutSchool />} />
-          <Route path="/music_department" element={<DepartmentPage id={'1'} showSelect={true} title={'Музичне відділення'}/>} />
-          <Route path="/vocal_choral_department" element={<DepartmentPage id={'2'} showSelect={true} title={'Вокально-хорове відділення'}/>} />
-          <Route path="/сhoreographic_department" element={<DepartmentPage id={'3'} showSelect={true} title={'Хореографічне відділення'}/>} />
-          <Route path="/theater_department" element={<DepartmentPage id={'4'} showSelect={false} title={'Образотворче відділення'}/>}/>
-          <Route path="/fine_arts_department" element={<DepartmentPage id={'5'} showSelect={true} title={'Театральне відділення'}/>}/>
-          <Route path="/preschool_preparatory_department" element={<DepartmentPage id={'6'} showSelect={false} title={'Дошкільне та підготовче відділення'}/>}/>
-          <Route path="/about_school_administration"element={<AboutSchool />}/>
+          <Route
+            path="/music_department"
+            element={
+              <DepartmentPage
+                id={'1'}
+                showSelect={true}
+                title={'Музичне відділення'}
+              />
+            }
+          />
+          <Route
+            path="/vocal_choral_department"
+            element={
+              <DepartmentPage
+                id={'2'}
+                showSelect={true}
+                title={'Вокально-хорове відділення'}
+              />
+            }
+          />
+          <Route
+            path="/сhoreographic_department"
+            element={
+              <DepartmentPage
+                id={'3'}
+                showSelect={true}
+                title={'Хореографічне відділення'}
+              />
+            }
+          />
+          <Route
+            path="/theater_department"
+            element={
+              <DepartmentPage
+                id={'4'}
+                showSelect={true}
+                title={'Театральне відділення'}
+              />
+            }
+          />
+          <Route
+            path="/fine_arts_department"
+            element={
+              <DepartmentPage
+                id={'5'}
+                showSelect={true}
+                title={'Образотворче відділення'}
+              />
+            }
+          />
+          <Route
+            path="/preschool_preparatory_department"
+            element={
+              <DepartmentPage
+                id={'6'}
+                showSelect={false}
+                title={'Дошкільне та підготовче відділення'}
+              />
+            }
+          />
+          <Route
+            path="/about_school_administration"
+            element={<AboutSchool />}
+          />
           {/*<Route path="/theater_department" element={<Theater />} />*/}
         </Route>
 
-        <Route path="/login" element={<LoginLayout />}>
-        </Route>
-        
+        <Route path="/login" element={<LoginLayout />}></Route>
+
         <Route path="/admin" element={<AdminSharedLayout />}>
           <Route index element={<AdminDashboard />} />
           <Route path="sliders" element={<SlidersPageAdmin />} />
@@ -68,12 +123,15 @@ const App = () => {
           <Route path="gallery" element={<GalleryPageAdmin />} />
           <Route path="departments" element={<DepartmentsPageAdmin />} />
           <Route path="achievements" element={<OurAchievementsPageAdmin />} />
-          <Route path="administration" element={<SchoolAdministrationPageAdmin />} />
+          <Route
+            path="administration"
+            element={<SchoolAdministrationPageAdmin />}
+          />
           <Route path="contacts" element={<ContactsPageAdmin />} />
           <Route path="password" element={<ChangePasswordPageAdmin />} />
         </Route>
-        
-         <Route path="/admin/*" element={<NotFoundAdmin />} />
+
+        <Route path="/admin/*" element={<NotFoundAdmin />} />
       </Routes>
     </Router>
   );
