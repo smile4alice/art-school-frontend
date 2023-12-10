ebe48b4d0c359e06ba561f53a2d2ce98ab7ebe8b
Iryna Artymuk <artimyk@ukr.net>
2023-12-10 14:17:59 +0200

    feat(posters edit): add formik

--- src/components/admin-components/Posters/posterInput/PostesInput.jsx
+++ src/components/admin-components/Posters/posterInput/PostesInput.jsx
@@ -1,38 +1,17 @@
 import TextArea from '@/components/admin-components/formElement/TextArea/TextArea';
 import FileInput from '@/components/admin-components/formElement/FileInput/FileInput';
 import styles from './PosterInput.module.scss';
-import { useParams } from 'react-router';
-import usePostersStore from '@/store/posterStore';
-import { useEffect, useState } from 'react';
 
 const PostersInput = ({ label }) => {
-  const { id } = useParams();
-  const { getPostersById } = usePostersStore();
-  const [poster, setPoster] = useState();
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const result = await getPostersById(id);
-
-        setPoster(result);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchData();
-  }, [getPostersById, id]);
-
   return (
     <div className={styles.layout}>
       <TextArea
         label="Заголовок"
         maxLength={120}
         errorMessage="Текст перевищує 2000 символів"
-        prevtext={poster?.title || ''}
       />
 
-      <FileInput label={label} prevImage={poster?.photo} />
+      <FileInput label={label} />
     </div>
   );
 };
--- src/pages/admin-pages/PostersAdmin/EditPostersPageAdmin.jsx
+++ src/pages/admin-pages/PostersAdmin/EditPostersPageAdmin.jsx
@@ -1,25 +1,86 @@
-import ButtonSubmit from '@/components/admin-components/Buttons/SubmitButton/ButtonSubmit';
-import PageTitle from '@/components/admin-components/PageTitle/PageTitle';
-import PostersInput from '@/components/admin-components/Posters/posterInput/PostesInput';
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
+import TextInput from '@/components/admin-components/formik/TextInput/TextInput';
+import TextArea from '@/components/admin-components/formik/TextArea/TextArea';
 
+import FileInput from '@/components/admin-components/formik/FileInput/FileInput';
+import { useEffect, useState } from 'react';
+import usePostersStore from '@/store/posterStore';
+import { useParams } from 'react-router';
+// import FormikContainer from '@/components/admin-components/formik/FormikContainer';
+
+const validationSchema = Yup.object({
+  // title: Yup.string().required('Yup Required').max(10),
+});
+
+const initialValues = {
+  title: '',
+  text: '',
+  image: [],
+};
 
 const EditPostersPage = () => {
+  const { id } = useParams();
+  const { getPostersById } = usePostersStore();
+  const [poster, setPoster] = useState();
+  console.log('poster: ', poster);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await getPostersById(id);
+
+        setPoster(result);
+
+        initialValues.title = result.title;
+        initialValues.photo = result.photo;
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, [getPostersById, id]);
+
+  // useEffect(() => {
+  //   initialValues.title = poster.title;
+  //   initialValues.photo = poster.photo;
+  // }, [poster]);
+  const onSubmit = values => {
+    console.log(values);
+  };
 
   return (
-    <div>
-      <PageTitle
-        title="Додати афішу"
-        showBackButton={true}
-        backButtonLink="/admin/sliders"
-        showActionButton={false}
-      />
-      <PostersInput />
-      <ButtonSubmit
-        nameButton="Зберегти зміни"
-        isActive={true}
-        isRight={true}
-      />
-    </div>
+    // <FormikContainer />
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+    >
+      <Form>
+        <div className="formControl">
+          <Field
+            name="title"
+            id="title"
+            placeholder="Title"
+            component={TextInput}
+            maxLength={10}
+            showCharacterCount={true}
+          />
+        </div>
+        <div className="formControl">
+          <Field
+            name="text"
+            id="title"
+            placeholder="Title"
+            component={TextArea}
+          />
+        </div>
+        <div className="formControl">
+          <Field name="image" id="image" component={FileInput} />
+        </div>
+        <button type="submit">Submit</button>
+      </Form>
+    </Formik>
   );
 };
 
