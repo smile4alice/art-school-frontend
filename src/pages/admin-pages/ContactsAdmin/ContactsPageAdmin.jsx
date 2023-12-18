4e5d4a9ba48affc9177108f78ed060bb2321fd76
cold <kholodihor@gmail.com>
2023-12-18 13:44:03 +0200

    feat(admin): add contacts section to admin panel

--- src/pages/admin-pages/ContactsAdmin/ContactsPageAdmin.jsx
+++ src/pages/admin-pages/ContactsAdmin/ContactsPageAdmin.jsx
@@ -1,6 +1,37 @@
+import { useState, useEffect } from 'react';
+import useContactsStore from '@/store/contactsStore';
+import PageTitle from '@/components/admin-components/PageTitle/PageTitle';
+import ContactsTable from '@/components/admin-components/Сontacts/ContactsTable/ContactsTable';
+import BreadCrumbs from '@/components/admin-components/BreadCrumbs/BreadCrumbs';
+
+const breadcrumbs = ['Контакти'];
+
 const ContactsPageAdmin = () => {
+  const { getContacts } = useContactsStore();
+  const [contacts, setContacts] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await getContacts();
+        setContacts(result);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, [getContacts]);
+
   return (
-    <div>Contacts Page Admin</div>
+    <div>
+      <BreadCrumbs breadcrumbs={breadcrumbs} />
+      <PageTitle
+        title="Контакти"
+        showBackButton={false}
+        showActionButton={false}
+      />
+      <ContactsTable data={contacts} />
+    </div>
   );
 };
 
