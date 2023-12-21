a7e703541294471193451eb0164d8d936de147d0
Iryna Artymuk <artimyk@ukr.net>
2023-12-21 16:25:21 +0200

    feat(password change): add password input component

--- src/pages/admin-pages/ChangePassword/ChangePasswordPageAdmin.jsx
+++ src/pages/admin-pages/ChangePassword/ChangePasswordPageAdmin.jsx
@@ -1,6 +1,73 @@
+import BreadCrumbs from '@/components/admin-components/BreadCrumbs/BreadCrumbs';
+import ButtonSubmit from '@/components/admin-components/Buttons/SubmitButton/ButtonSubmit';
+import PageTitle from '@/components/admin-components/PageTitle/PageTitle';
+import PasswordInput from '@/components/admin-components/formik/PasswordInput/PasswordInput';
+import TextInput from '@/components/admin-components/formik/TextInput/TextInput';
+import { Field, Form, Formik } from 'formik';
+import styles from './ChangePassword.module.scss';
+
+const breadcrumbs = ['Зміна паролю'];
+const initialValues = {
+  oldPassword: '',
+  newPassword: '',
+  confirmPassword: [],
+};
 const ChangePasswordPageAdmin = () => {
   return (
-    <div>Change password</div>
+    <div>
+      <BreadCrumbs breadcrumbs={breadcrumbs} />
+      <PageTitle
+        title="Зміна паролю"
+        showBackButton={false}
+        showActionButton={false}
+      />
+
+      <Formik
+        initialValues={initialValues}
+        // validationSchema={newsValidation}
+        // onSubmit={onSubmit}
+      >
+        {formik => {
+          return (
+            <Form>
+              <div className={styles.layout}>
+                <Field
+                  name="oldPassword"
+                  id="oldPassword"
+                  placeholder="Поточний пароль"
+                  component={TextInput}
+                  label="Поточний пароль*"
+                />
+                <Field
+                  name="newPassword"
+                  id="newPassword"
+                  placeholder="Новий пароль"
+                  component={PasswordInput}
+                  label="Новий пароль*"
+                />
+                <Field
+                  name="confirmPassword"
+                  id="confirmPassword"
+                  placeholder="Повторити новий пароль"
+                  component={PasswordInput}
+                  label="Повторити новий пароль*"
+                />
+
+                <div className={styles.button}>
+                  <ButtonSubmit
+                    nameButton="Зберегти зміни"
+                    isActive={formik.isValid}
+                    isRight={true}
+                    // handlerSubmitButton={onSubmit}
+                    // isProcessing={isProcessing}
+                  />
+                </div>
+              </div>
+            </Form>
+          );
+        }}
+      </Formik>
+    </div>
   );
 };
 
