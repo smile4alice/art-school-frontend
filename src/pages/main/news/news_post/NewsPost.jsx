8082a940ace79fb6414bd1a248c57b757e83556d
Iryna Artymuk <artimyk@ukr.net>
2023-12-27 17:44:08 +0200

    fix(news  post): fix render error

--- src/pages/main/news/news_post/NewsPost.jsx
+++ src/pages/main/news/news_post/NewsPost.jsx
@@ -2,19 +2,18 @@ import { useLocation } from 'react-router-dom';
 
 import Container from '@/components/Container/Container';
 
-
 import styles from './NewsPost.module.scss';
 import NavLinkButton from '@/components/ui/Buttons/NavLinkButton';
 
 const NewsPost = () => {
   const location = useLocation();
   const { post } = location.state;
-   <NavLinkButton href={href} text={'переглянути всі новини'} />;
+
   return (
     <Container>
       <section className={styles.wrapper}>
         <div className={styles.buttonContainer}>
-          <NavLinkButton href={href} text={'переглянути всі новини'} />
+          <NavLinkButton href={'/news'} text={'переглянути всі новини'} />
         </div>
         <p className={`${styles.title} sectionTitle`}>{post.title}</p>
         <p className={styles.date}>{post.date}</p>
