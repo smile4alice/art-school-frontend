0a0887841566296286a19643ee82ab2a3a74e871
Iryna Artymuk <artimyk@ukr.net>
2023-12-27 14:03:27 +0200

    feat(add link to school history): add link to school history home page

--- src/components/main/History/History.jsx
+++ src/components/main/History/History.jsx
@@ -1,10 +1,10 @@
 import Container from '@/components/Container/Container';
 import NavLinkButton from '@/components/ui/Buttons/DownloadButton';
-
+import historyData from '@/data/about/history.json';
 import styles from './History.module.scss';
 
 const History = () => {
-  const url = 'https://www.example.com';
+  const url = '/about_school_history';
   const buttonName = 'Читати більше';
 
   return (
@@ -12,18 +12,10 @@ const History = () => {
       <div className={styles.wrapper}>
         <div className={styles.info}>
           <p className={`${styles.title} sectionTitle`}>Історія школи</p>
-          <p className={styles.text}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur.Lorem ipsum dolor sit amet, consectetur adipiscing elit, do
-            tempor...
-          </p>
+          <p className={styles.text}>{historyData[0].description}</p>
         </div>
         <div className={styles.buttonContainer}>
-          <NavLinkButton title={buttonName} link={url} />
+          <NavLinkButton title={buttonName} to={url} />
         </div>
         <div className={styles.img_container}>
           <img src="/school.png" alt="school-building" />
