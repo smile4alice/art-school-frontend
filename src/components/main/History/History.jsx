ae07f761b067325662ca7c11a321959054fcd091
cold <kholodihor@gmail.com>
2024-01-23 16:55:39 +0200

    fix: fix bugs

--- src/components/main/History/History.jsx
+++ src/components/main/History/History.jsx
@@ -1,10 +1,11 @@
+import { Link } from 'react-router-dom';
 import Container from '@/components/Container/Container';
 import NavLinkButton from '@/components/ui/Buttons/DownloadButton';
 import historyData from '@/data/about/history.json';
 import styles from './History.module.scss';
 
 const History = () => {
-  const url = '/about_school_history';
+  const url = '/about-school-history';
   const buttonName = 'Читати більше';
 
   return (
@@ -16,7 +17,9 @@ const History = () => {
             <p className={styles.text}>{historyData[0].description}</p>
           </div>
           <div className={styles.buttonContainer}>
-            <NavLinkButton title={buttonName} to={url} />
+            <Link to={url}>
+              <NavLinkButton title={buttonName} to={url} />
+            </Link>
           </div>
           <div className={styles.img_container}>
             <img
