f21eaa5503624b9b0a4311441d3514a58d787d0b
cold <kholodihor@gmail.com>
2024-01-29 13:14:16 +0200

    fix: fix bug

--- src/pages/admin-pages/SlidersAdmin/SlidersPageAdmin.jsx
+++ src/pages/admin-pages/SlidersAdmin/SlidersPageAdmin.jsx
@@ -1,4 +1,6 @@
 import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAuthorized } from '@/store/IsAuthorizedStore';
 import useSlidersStore from '@/store/slidersStore';
 
 import PageTitle from '@/components/admin-components/PageTitle/PageTitle';
@@ -7,13 +9,25 @@ import BreadCrumbs from '@/components/admin-components/BreadCrumbs/BreadCrumbs';
 import SpinnerAdmin from '@/components/admin-components/SpinnerAdmin/SpinnerAdmin';
 import PlaceholderAdmin from '@/components/admin-components/PlaceholderAdmin/PlaceholderAdmin';
 
+import styles from './SlidersAdmin.module.scss';
+
 const breadcrumbs = ['Слайдери'];
 
 const SlidersPageAdmin = () => {
   const { getSlides } = useSlidersStore();
+  const { setUnAuthorized } = useAuthorized();
+  const navigate = useNavigate();
   const slides = useSlidersStore(state => state.slides);
   const loading = useSlidersStore(state => state.loading);
   const error = useSlidersStore(state => state.error);
+  const isAuthorized = useSlidersStore(state => state.isAuthorized);
+
+  useEffect(() => {
+    if (isAuthorized) return;
+    localStorage.removeItem('access_token');
+    setUnAuthorized();
+    navigate('/login');
+  }, [isAuthorized, navigate, setUnAuthorized]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -37,12 +51,15 @@ const SlidersPageAdmin = () => {
         isActionButtonDisabled={slides.length >= 8}
         actionButtonLabel="Додати слайд"
       />
+      {error && <p className={styles.error}>{error}</p>}
       {loading && !Object.keys(error).length ? (
         <SpinnerAdmin />
       ) : (
         <SlidersTable data={slides} />
       )}
-      {error && Object.keys(error).length ? <PlaceholderAdmin /> : null}
+      {error && Object.keys(error).length && !slides.length ? (
+        <PlaceholderAdmin />
+      ) : null}
     </div>
   );
 };
