22f3dc88f269404677eeb9db6677c5aa5be7ba13
cold <kholodihor@gmail.com>
2024-02-08 12:20:56 +0200

    fix: fix video swiping

--- src/components/main/News/News.jsx
+++ src/components/main/News/News.jsx
@@ -11,6 +11,7 @@ import 'swiper/css';
 import Spinner from '@/components/ui/Spinner/Spinner';
 import useVideoStore from '@/store/videoStore';
 import Select from '@/components/ui/Select/Select';
+import Navigation from './Navigation/Navigation';
 
 const News = ({ selectOptions }) => {
   const swiperRef = useRef();
@@ -19,7 +20,6 @@ const News = ({ selectOptions }) => {
   const videos = useVideoStore(state => state.videos);
   const [loadingState, setLoadingState] = useState('loading');
   const [departmentId, setDepartmentId] = useState(selectOptions?.[0].id);
-  const [page, setPage] = useState(1);
   const changeDepartment = id => {
     setDepartmentId(id);
   };
@@ -105,6 +105,10 @@ const News = ({ selectOptions }) => {
               <Placeholder />
             </div>
           )}
+          <Navigation
+            onPrevClick={() => swiperRef.current.slidePrev()}
+            onNextClick={() => swiperRef.current.slideNext()}
+          />
 
           {departmentId && !isLaptop && (
             <Select
